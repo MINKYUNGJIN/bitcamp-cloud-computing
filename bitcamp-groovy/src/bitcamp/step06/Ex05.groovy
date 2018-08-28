1ac96b4bd1cb0614ab66fd85c0185447019e4097
MINKYUNGJIN <mkjin1201@gmail.com>
2018-08-28 16:42:17 +0900

    .

---
+++ bitcamp-groovy/src/bitcamp/step06/Ex05.groovy
@@ -0,0 +1,13 @@
+// 문자열 다루기 - GString
+package bitcamp.step06
+
+name = "홍길동"
+str1 = "이름은 $name 입니다."
+str2 = "이름은" + name + "입니다."
+
+println str1 instanceof String
+println str1 instanceof GString
+
+
+println str2 instanceof String
+println str2 instanceof GString
